7cad4bb4507a4b83b47264d31c0e7dfe0035bff9
huiliang.zhang <huiliang.zhang@jinshuschool.com>
2025-11-26 17:00:05 +0800

    108

---
+++ 0108_binary_search_tree.ts
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
+    }
+}
+
+function sortedArrayToBST(nums: number[]): TreeNode | null {
+    if (!nums.length) {
+        return null;
+    }
+    const mid = Math.floor(nums.length / 2);
+    return new TreeNode(nums[mid], sortedArrayToBST(nums.slice(0, mid)), sortedArrayToBST(nums.slice(mid + 1)));
+};
+
+
+console.log(sortedArrayToBST([-10,-3,0,5,9]));
